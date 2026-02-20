4a7778c2f7953b8526a4edd4646a75a970d69bd5
Eduardo Guimarães <139907650+guimaraesdeol@users.noreply.github.com>
2026-02-20 17:24:35 -0400

    Mudando

--- script.js
+++ script.js
@@ -181,7 +181,7 @@ checkoutBtn.addEventListener("click", function(){
 function checkRestaurantOpen(){
     const data = new Date();
     const hora = data.getHours();
-    return hora >= 18 && hora < 24; // boolean true/false
+    return hora >= 0 && hora < 24; // boolean true/false
 
     
 }
